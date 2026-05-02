34fb30efe0605aa04127bb0bb6e5a476b331b42f
Zelimkhan <zelim8458@gmail.com>
2026-05-02 22:49:29 +0200

    Create annonceillegal.js

---
+++ commands/annonceillegal.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('discord.js');
+const sendAnnouncement = require('../utils/sendAnnouncement');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('annonceillegal')
+    .setDescription('Envoyer une annonce pour le rôle Notification Illégal')
+    .addStringOption(option =>
+      option
+        .setName('message')
+        .setDescription('Message de l’annonce')
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    await sendAnnouncement(interaction, {
+      commandName: 'annonceillegal',
+      mentionType: 'role',
+      roleName: 'Notification Illégal',
+      title: 'Annonce Illégal',
+      emoji: '🛡️',
+      color: 0xed4245
+    });
+  }
+};
