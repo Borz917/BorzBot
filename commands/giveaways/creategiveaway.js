cfdcf81a154dd86c39895df7cd3e5e4ae1015e0e
Zelimkhan <zelim8458@gmail.com>
2026-05-22 15:30:56 +0200

    Delete commands directory

--- commands/giveaways/creategiveaway.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const reply = require('../../utils/reply');
-module.exports = {
-  data: new SlashCommandBuilder().setName("creategiveaway").setDescription("Commande creategiveaway.").setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-  async execute(interaction) {
-    return reply.success(interaction, 'Commande exécutée', `✅ Commande /creategiveaway exécutée.`, { ephemeral: false });
-  }
-};
--- commands/giveaways/endgiveaway.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const reply = require('../../utils/reply');
-module.exports = {
-  data: new SlashCommandBuilder().setName("endgiveaway").setDescription("Commande endgiveaway.").setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
-  async execute(interaction) {
-    return reply.success(interaction, 'Commande exécutée', `✅ Commande /endgiveaway exécutée.`, { ephemeral: false });
-  }
-};
